80894e6afdad8d1a989e96ea27e3d3738916e2ff
mahender reddy <mahender reddy@192.168.100.4>
2019-05-17 14:01:49 +0530

    complete

--- src/main/java/main/TicTacToe.java
+++ src/main/java/main/TicTacToe.java
@@ -13,13 +13,32 @@ public static int[][] ticbox;
 		//loop through columns from 0 to 3 and check if all the 3 places have same marks
  		
 		//check both diagonals 
-		
-		
-		
-		//write your code here !!!
-		
-		
-		
+		for(int row=0;row<3;row++)
+		{
+			if(tic[row][0]==1 && tic[row][1]==1 && tic[row][2]==1)
+				return 1;
+			if(tic[row][0]==2 && tic[row][1]==2 && tic[row][2]==2)
+				return 2;	
+		}
+		for(int col=0;col<3;col++)
+		{
+			if(tic[0][col]==1 && tic[0][col]==1 && tic[0][col]==1)
+				return 1;
+			if(tic[0][col]==2 && tic[0][col]==2 && tic[0][col]==2)
+				return 2;		
+		}
+		if(tic[0][0]==1 && tic[1][1]==1 && tic[2][2]==1)
+         
+            return 1;
+
+        if(tic[0][0]==2 && tic[1][1]==2 && tic[2][2]==2)  
+
+            return 2;
+        if(tic[0][2]==1 && tic[1][1]==1 && tic[2][0]==1)
+		    return 1;
+		if(tic[0][2]==2 && tic[1][1]==2 && tic[2][0]==2)
+            return 2;
+            
 		return 0;
 		
 	}
@@ -37,11 +56,13 @@ public static int[][] ticbox;
 		//check if a2 is between 0 & 3
 		//check if the selected box is empty ie, already not marked by other player
 		//if all checks passed return true.
-		
-		
-		//Write your code here !!!
-		
-		
+		if((0<=a1 && a1<3) && (0<=a2 && a2<3))
+		{
+			if(tic[a1][a2]==0)
+			{
+				return true;
+			}
+		}
 		return false;
 	}
 	public static void main(String args[]) {
